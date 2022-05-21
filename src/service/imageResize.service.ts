a22ea6623e354cc8166576bf0ca0b0ffaeffda35
alsabagh-dev <zizo.naser@stud.fci-cu.edu.eg>
2022-05-21 13:27:05 +0200

    fix: Separating out image processing function.

---
+++ src/service/imageResize.service.ts
@@ -0,0 +1,33 @@
+import sharp from 'sharp';
+
+import * as fs from 'fs';
+import * as path from 'path';
+import { promisify } from 'util';
+
+const readFile = promisify(fs.readFile);
+
+export type ImageResizeConfig = {
+    input: string,
+    output: string,
+    width: number,
+    height: number
+}
+
+export const imageResize = async (config: ImageResizeConfig): Promise<void> => {
+    try {
+        const image = await readFile(
+            path.join(__dirname, '../../assets/full/', config.input + '.jpg')
+        );
+        const outputImage = path.join(__dirname, '../../assets/thumbnails/',config.output);
+        await sharp(image)
+            .resize({
+                fit: sharp.fit.contain,
+                height: config.height,
+                width: config.width
+            })
+            .toFile(outputImage);
+    } catch (error) {
+        throw error;
+    }
+}
+
